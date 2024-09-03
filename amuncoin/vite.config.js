eb473b5576a1d66d096cfa93caeaa7c8c60bb2ad
MilosGlamocak <milosglamocak@gmail.com>
2024-09-03 21:31:11 +0200

    added github pages

--- amuncoin/vite.config.js
+++ amuncoin/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/crypto-landing/', // The repository name or path on GitHub Pages
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
